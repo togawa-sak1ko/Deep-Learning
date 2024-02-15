bbcfc4a0d2a6112786193812bbe6b545e4e0f2e8
togawa-sak1ko <154679993+togawa-sak1ko@users.noreply.github.com>
2024-02-15 22:53:28 +0800

    Add files via upload

--- 08.py
+++ 08.py
@@ -5,19 +5,21 @@ from d2l import torch as d2l
 
 def synthetic_data(w, b, num_examples):
     x = torch.normal(0, 1, (num_examples, len(w)))
-    y = torch.matmul(x, w)+b
+    y = torch.matmul(x, w) + b
     y += torch.normal(0, 0.01, y.shape)
     return x, y.reshape((-1, 1))
 
 true_w = torch.tensor([2, -3.4])
 true_b = 4.2
 features, labels = synthetic_data(true_w, true_b, 1000)
+#print(features)
+#print(labels)
 
 d2l.set_figsize()
 d2l.plt.scatter(features[:, 1].detach().numpy(), labels.detach().numpy(), 1)
 plt.show()
 
-def date_iter(batch_size, features, labels):
+def data_iter(batch_size, features, labels):
     num_examples = len(features)
     indices = list(range(num_examples))
     random.shuffle(indices)
@@ -27,6 +29,36 @@ def date_iter(batch_size, features, labels):
 
 batch_size = 10
 
-for x,y in date_iter(batch_size, features, labels):
-    print(x, '\n', y)
-    break
+#for x, y in data_iter(batch_size, features, labels):
+#    print(x, '\n', y)
+#    break
+
+w = torch.normal(0, 0.01, size=(2,1), requires_grad = True)
+b = torch.zeros(1, requires_grad = True)
+
+def linreg(x, w, b):
+    return torch.matmul(x, w) + b
+
+def squared_loss(y_hat, y):
+    return (y_hat - y.reshape(y_hat.shape))**2 / 2
+
+def sgd(params, lr, batch_size):
+    with torch.no_grad():
+        for param in params:
+            param -= lr * param.grad / batch_size
+            param.grad.zero_()
+
+lr = 0.03
+num_epochs = 3
+net = linreg
+loss = squared_loss
+
+for epoch in range(num_epochs):
+    for x, y in data_iter(batch_size, features, labels):
+        l = loss(net(x, w, b), y)
+        l.sum().backward()
+        sgd([w, b], lr, batch_size)
+    with torch.no_grad():
+        train_1 = loss(net(features, w, b), labels)
+        print(f'epoch {epoch + 1}, loss {float(train_1.mean()):f}')
+
