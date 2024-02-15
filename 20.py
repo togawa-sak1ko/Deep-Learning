16c1cde2ff13d4ab33242b3a1a3bb11152a148ee
togawa-sak1ko <154679993+togawa-sak1ko@users.noreply.github.com>
2024-02-15 23:40:20 +0800

    Add files via upload

---
+++ 20.py
@@ -0,0 +1,20 @@
+import torch
+from torch import nn
+
+def comp_conv2d(conv2d, x):
+    x = x.reshape((1,1)+x.shape)
+    y = conv2d(x)
+    return y.reshape(y.shape[2:])
+
+conv2d = nn.Conv2d(1,1,kernel_size=3,padding=1)
+x = torch.rand((8,8))
+print(comp_conv2d(conv2d,x).shape)
+
+conv2d = nn.Conv2d(1,1,kernel_size=(5,3),padding=(2,1))
+print(comp_conv2d(conv2d,x).shape)
+
+conv2d = nn.Conv2d(1,1,kernel_size=3,padding=1,stride=2)
+print(comp_conv2d(conv2d,x).shape)
+
+conv2d = nn.Conv2d(1,1,kernel_size=(3,5),padding=(0,1),stride=(3,4))
+print(comp_conv2d(conv2d,x).shape)
